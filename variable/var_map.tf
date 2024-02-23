b8fcd93ac0bc260a44159a5e0abaacab67ed9a5b
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 13:00:42 +0530

    variable_map_function

--- variable/var_map.tf
+++ variable/var_map.tf
@@ -7,6 +7,10 @@ variable "map" {
     }
 }
 
+variable "username" {
+    type = string
+}
+
 output "map_lookup_function" {
-    value = "My name is vishal ${lookup(var.map,"vishal")}"
+    value = "My name is ${var.username} ${lookup(var.map,"${var.username}")}"
 }
