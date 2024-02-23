96c1cbe9a6020fd21cb3c3e833bd115a27ab6cc6
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 13:30:54 +0530

    enviroment_variable_with_custom_tfvars_file

--- custom_variable_file/main.tf
+++ custom_variable_file/main.tf
@@ -1,13 +1,13 @@
 #using custom variable file change terraform.tfvars with any name using .tfvars extention
 #run file using these command ---->terraform plan -var-file "file_name.tfvars"
-variable "age" {
-    type = number
-}
+# variable "age" {
+#     type = number
+# }
 
-variable "username" {
-    type = string
-}
+# variable "username" {
+#     type = string
+# }
 
-output "map_lookup_function" {
-    value = "My name is ${var.username} My age is ${var.age}"
-}
+# output "map_lookup_function" {
+#     value = "My name is ${var.username} My age is ${var.age}"
+# }
