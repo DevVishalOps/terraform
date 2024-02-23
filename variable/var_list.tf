25cc5a93fac476b1ed633cdb6354956d841a5afe
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 12:32:42 +0530

    variable_list

--- variable/var_list.tf
+++ variable/var_list.tf
@@ -6,11 +6,3 @@ variable "list" {
 output "list_1st_output" {
     value = "${var.list[0]}"
 }
-
-output "list_2nd_output" {
-    value = "${var.list[1]}"
-}
-
-output "list_3rd_output" {
-    value = "${var.list[2]}"
-}
