9c1fe6152698cb6caa398a528050c03a5f12304d
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 12:42:13 +0530

    variable_list_function

---
+++ variable/var_function.tf
@@ -0,0 +1,9 @@
+#list using builtin functions print
+variable "list" {
+    type = list
+    default = ["vishal","sai"]
+}
+
+output "list_1st_output" {
+    value = "${join(",",var.list)}"
+}
--- variable/var_list.tf
+++ variable/var_list.tf
@@ -1,9 +1,9 @@
 #varible using list and print index wise it useful for security group
-variable "list" {
-    type = list
-    default = ["vishal","sai"]
-}
+# variable "list" {
+#     type = list
+#     default = ["vishal","sai"]
+# }
 
-output "list_1st_output" {
-    value = "${var.list[0]}"
-}
+# output "list_1st_output" {
+#     value = "${var.list[0]}"
+# }
