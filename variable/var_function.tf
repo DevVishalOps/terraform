1c4f91c9f90dc993254dee73e5d544fe8a43d639
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 12:44:43 +0530

    variable_list_function_update1

--- variable/var_function.tf
+++ variable/var_function.tf
@@ -4,6 +4,10 @@ variable "list" {
     default = ["vishal","sai"]
 }
 
-output "list_1st_output" {
+output "list_join_function" {
     value = "${join("----->",var.list)}"
+}
+
+output "list_upper_function" {
+    value = "${upper(var.list)}"
 }
