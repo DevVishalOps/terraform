688fa4f111ac0747526a5df4cbf269e9173ac4e9
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 14:46:08 +0530

    structure_project2

--- structure_project/instance.tf
+++ structure_project/instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "Structure_project" {
+resource "aws_instance" "project" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
