a76001aacb061e69d71dd525cbf0ff9b0cfed520
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 22:44:12 +0530

    my_first_project

--- structure_project/instance.tf
+++ structure_project/instance.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "myproject" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups = ["aws_security_group"]
+    security_groups = ["aws_security_group.allow_tls"]
     tags = var.tags_names
 }
