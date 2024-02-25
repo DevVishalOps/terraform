269044fd10cf40030953eca388c99c24e39eb74f
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 14:20:33 +0530

    structure_project

---
+++ structure_project/terraform.tfvars
@@ -0,0 +1,11 @@
+ports = [22,80,443,3306]
+
+ami_id = "ami-03f4878755434977f"
+
+instance_type = "t2.micro"
+
+key_name = "mkey"
+
+tags_names = {
+      Name = "Structure Project"
+    }
---
+++ structure_project/variables.tf
@@ -0,0 +1,19 @@
+variable "ports" {
+    type = list(number)
+}
+
+variable "ami_id" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "tags_names" {
+  type = map(string)
+}
