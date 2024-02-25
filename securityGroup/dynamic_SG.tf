a81ff06ab45fcc11d8251047605b6ce8b4a2ee97
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 13:51:14 +0530

    EC2_SG

--- securityGroup/dynamic_SG.tf
+++ securityGroup/dynamic_SG.tf
@@ -1,20 +1,20 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "Terraform first SG"
-  description = "Allow TLS inbound traffic"
+# resource "aws_security_group" "allow_tls" {
+#   name        = "Terraform first SG"
+#   description = "Allow TLS inbound traffic"
 
-  dynamic "ingress" {
-    for_each = [22,80,443,3306]
-    iterator = port
-    content {
-        description = "TLS from VPC"
-        from_port = port.value
-        to_port = port.value
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-}
+#   dynamic "ingress" {
+#     for_each = [22,80,443,3306]
+#     iterator = port
+#     content {
+#         description = "TLS from VPC"
+#         from_port = port.value
+#         to_port = port.value
+#         protocol = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+# }
 
-output "securityGroupDetatils" {
-    value = "${aws_security_group.allow_tls.id}"
-}
+# output "securityGroupDetatils" {
+#     value = "${aws_security_group.allow_tls.id}"
+# }
