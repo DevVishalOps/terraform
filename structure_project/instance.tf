a8933ffb0b7b32454ab7fcc2954526064734d696
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 14:36:54 +0530

    structure_project1

--- structure_project/instance.tf
+++ structure_project/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "Structure_project" {
     ami = "${var.ami_id}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
