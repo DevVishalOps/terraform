c5d67d1d0862420c22cb9929430c25f1939c05cb
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 14:38:54 +0530

    structure_project2

--- structure_project/instance.tf
+++ structure_project/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Structure_project" {
-    ami = "${var.ami_id}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.key_name}"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
     vpc_security_group_ids = ["aws_security_group.allow_tls"]
     tags = var.tags_names
 }
