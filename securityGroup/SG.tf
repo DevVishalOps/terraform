5591d25cf11f27c590bedfa297ecddc53a0fa6c0
vishal <rajput.vishal.rajes@gmail.com>
2024-02-25 13:32:41 +0530

    dynamic_SG_ingress

--- securityGroup/SG.tf
+++ securityGroup/SG.tf
@@ -1,34 +1,34 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "Terraform first SG"
-  description = "Allow TLS inbound traffic"
+# resource "aws_security_group" "allow_tls" {
+#   name        = "Terraform first SG"
+#   description = "Allow TLS inbound traffic"
 
-  ingress {
-    description = "TLS from VPC"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "TLS from VPC"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port = 80
+#     to_port = 80
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port = 443
+#     to_port = 443
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port = 3306
+#     to_port = 3306
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
