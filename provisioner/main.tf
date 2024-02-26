6da63ce0e29b0416c697cc87f048e323ed1606d6
vishal <rajput.vishal.rajes@gmail.com>
2024-02-26 12:17:40 +0530

    provisioner_1st

---
+++ provisioner/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {   
+    region = "ap-south-1"
+}
+resource "aws_instance" "Provisioner" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    vpc_security_group_ids = ["${aws_security_group.nginxSG.id}"]
+    tags = var.tags_names
+
+    # 3 types of provisioner--> file,local-exec,remote-exec
+    provisioner "file" {
+    source = "readme.md"
+    destination = "/tmp/readme.md"
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("${path.module}/id_ed25519")
+      host = "${aws_instance.Provisioner.public_ip}"
+    }
+    }
+}
---
+++ provisioner/terraform.tfvars
@@ -0,0 +1,9 @@
+ami_id = "ami-03f4878755434977f"
+
+instance_type = "t2.micro"
+
+key_name = "mkey"
+
+tags_names = {
+      Name = "provisioner"
+    }
