d802268c899bb4752dad15600e2e146e43d7b488
vishal <rajput.vishal.rajes@gmail.com>
2024-02-26 12:48:02 +0530

    provisioner_4th

--- provisioner/main.tf
+++ provisioner/main.tf
@@ -8,15 +8,22 @@ resource "aws_instance" "Provisioner" {
     vpc_security_group_ids = ["${aws_security_group.provisionerSG.id}"]
     tags = var.tags_names
 
-    # 3 types of provisioner--> file,local-exec,remote-exec
-    provisioner "file" {
-    source = "readme.md"
-    destination = "/tmp/readme.md"
+    #connection block used for provisioner
     connection {
       type = "ssh"
       user = "ubuntu"
       private_key = file("${path.module}/id_ed25519")
       host = "${self.public_ip}"
     }
+    # 3 types of provisioner--> file,local-exec,remote-exec
+    provisioner "file" {
+    source = "security.tf"
+    destination = "/tmp/security.tf"
     }
+
+    provisioner "file" {
+      content = "This my content"
+      destination = "/tmp/content.md"
+    }
+
 }
