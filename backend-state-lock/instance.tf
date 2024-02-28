714b9a169d775c3a36770e7c58304dda6fcf1d1c
vishal <rajput.vishal.rajes@gmail.com>
2024-02-28 10:55:22 +0530

    backendwith-statelockfiles

---
+++ backend-state-lock/instance.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+        bucket = "my-terraform-backend-bucket-1"
+        region = "ap-south-1" 
+        key = "terraform.tfstate"
+        dynamodb_table = "my-state-lock-table"
+  }
+}
+
+
+resource "aws_instance" "backend" {
+    ami = "ami-03f4878755434977f"
+    instance_type = "t2.micro"
+}
