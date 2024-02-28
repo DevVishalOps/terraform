abef25b7b6cef085004690826684dbe410e979c1
vishal <rajput.vishal.rajes@gmail.com>
2024-02-28 10:28:54 +0530

    backendfiles

---
+++ backend/instance.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+        bucket = "my-terraform-backend-bucket-1"
+        region = "ap-south-1" 
+  }
+}
+
+
+resource "aws_instance" "backend" {
+    ami = "ami-03f4878755434977f"
+    instance_type = "t2.micro"
+}
