d6d392ba3b9b17151abcb9044724ec5727e0a57c
vishal <rajput.vishal.rajes@gmail.com>
2024-03-07 21:00:31 +0530

    key-pair

--- keypair/aws-kp.tf
+++ keypair/aws-kp.tf
@@ -2,3 +2,7 @@ resource "aws_key_pair" "key-tf" {
   key_name   = "key-tf"
   public_key = file("${path.module}/id_ed25519.pub")
 }
+
+output "key-name" {
+    value = "${aws_key_pair.key-tf.id}"
+}
