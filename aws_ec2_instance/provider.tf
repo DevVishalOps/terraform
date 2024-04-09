07be4fa1156ec579572b87a3054271ed1a3dfc66
vishal <rajput.vishal.rajesh@gmail.com>
2024-04-09 10:45:47 +0530

    providerconfi

--- aws_ec2_instance/provider.tf
+++ aws_ec2_instance/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    mycloud = {
-      source  = "mycorp/mycloud"
-      version = "~> 1.0"
-      configuration_aliases = [ mycloud.alternate ]
-    }
-  }
-}
-
-
 provider "aws" {
     region = "ap-south-1"
 }
