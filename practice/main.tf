605d1bd0a9bfc49bea3c155b7c1beb4ceddfb5f1
vishal <rajput.vishal.rajesh@gmail.com>
2024-04-14 11:35:33 +0530

    practicecommit

--- practice/main.tf
+++ practice/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-    region = "ap-south-1"
-}
-
 resource "aws_instance" "instance_1" {
     ami = "ami-03f4878755434977f"
     instance_type = "t2.micro"
