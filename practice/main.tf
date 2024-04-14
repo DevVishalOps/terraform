6c48a8fef31b7c5e745ab633d03a1cb7fae9508e
vishal <rajput.vishal.rajesh@gmail.com>
2024-04-14 11:31:51 +0530

    practicecommit

--- practice/main.tf
+++ practice/main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 resource "aws_instance" "instance_1" {
     ami = "ami-03f4878755434977f"
     instance_type = "t2.micro"
+    tags = {
+      name = "my-terraform-practice"
+    }
 }
